518218be0135e34decc0e16c7c95298482cfe2bd
Praveen <er.praveendewangan@gmail.com>
2021-05-15 21:31:12 +0530

    Questions on Queue

---
+++ algo/_codes/stack&Queue/_30QueueToStackPopEfficient.java
@@ -0,0 +1,77 @@
+import java.util.Queue;
+
+public class _30QueueToStackPopEfficient {
+    
+  public static class QueueToStackAdapter {
+    Queue<Integer> mainQ;
+    Queue<Integer> helperQ;
+
+    public QueueToStackAdapter() {
+      mainQ = new ArrayDeque<>();
+      helperQ = new ArrayDeque<>();
+    }
+
+    int size() {
+      return mainQ.size();
+    }
+
+    void push(int val) {
+    //   while(size() != 0) {
+    //       helperQ.add(mainQ.remove());
+    //   }
+    //   mainQ.add(val);
+    //   while(helperQ.size() != 0) {
+    //       mainQ.add(helperQ.remove());
+    //   }  
+        helperQ.add(val);
+        while(mainQ.size() != 0) {
+            helperQ.add(mainQ.remove());
+        }
+        mainQ = helperQ;
+        helperQ = new ArrayDeque<>();
+
+    }
+
+    int pop() {
+      if(size() == 0) {
+          System.out.println("Stack underflow");
+          return -1;
+      } 
+      return mainQ.remove();
+    }
+
+    int top() {
+      if(size() == 0) {
+          System.out.println("Stack underflow");
+          return -1;
+      } 
+      return mainQ.peek();
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    QueueToStackAdapter st = new QueueToStackAdapter();
+
+    String str = br.readLine();
+    while (str.equals("quit") == false) {
+      if (str.startsWith("push")) {
+        int val = Integer.parseInt(str.split(" ")[1]);
+        st.push(val);
+      } else if (str.startsWith("pop")) {
+        int val = st.pop();
+        if (val != -1) {
+          System.out.println(val);
+        }
+      } else if (str.startsWith("top")) {
+        int val = st.top();
+        if (val != -1) {
+          System.out.println(val);
+        }
+      } else if (str.startsWith("size")) {
+        System.out.println(st.size());
+      }
+      str = br.readLine();
+    }
+  }
+}
